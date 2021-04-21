f0577eeb0b8060838b9d732fdc84c490803a987b
proggcreator <davidshibitov@gmail.com>
2021-04-21 18:51:47 +0300

    disign

--- wordsapp/models.py
+++ wordsapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Topic(models.Model):
     name = models.CharField("Тема", max_length = 30)
     description = models.TextField("Описание")
-    urls = models.SlugField(max_length = 160, unique = True)
+    urls = models.TextField()
 
     def __str__(self):
         return self.name
